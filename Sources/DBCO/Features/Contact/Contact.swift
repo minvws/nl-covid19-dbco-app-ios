5f126d30a59be438849b2af37420b0569f1c211d
Thom Hoekstra <thomhoekstra@live.nl>
2020-09-29 11:06:07 +0200

    DBCO-62: Move FieldType code to own file

--- Sources/DBCO/Features/Contact/Contact.swift
+++ Sources/DBCO/Features/Contact/Contact.swift
@@ -8,6 +8,35 @@
 import Foundation
 import Contacts
 
+enum ContactType {
+    case roommate
+    case close
+    case general
+    case other
+}
+
+enum ContactField {
+    case firstName
+    case lastName
+    case phoneNumber
+    case email
+    case relation
+    case birthDate
+    case bsn
+    case profession
+    case companyName
+    case notes
+    
+    var allowsMultiple: Bool {
+        switch self {
+        case .phoneNumber, .email:
+            return true
+        default:
+            return false
+        }
+    }
+}
+
 struct Contact {
     var type: ContactType
     var values: [(field: ContactField, value: String?, identifier: UUID)]
@@ -33,91 +62,12 @@ struct Contact {
             .joined(separator: " ")
     }
     
-    var isValid: Bool {
-        type.requiredFields.allSatisfy { field in
-            values.contains {
-                $0.field == field && $0.value?.isValid(for: field.fieldType) == true
-            }
-        }
-    }
-    
     mutating func setValue(_ value: String?, forFieldWithIdentifier identifier: UUID) {
         guard let index = values.firstIndex(where: { $0.identifier == identifier }) else {
             return
         }
         
         values[index].value = value
-    }
-}
-
-enum Validation {
-    case name
-    case email
-    case phoneNumber
-    case number
-    case general
-}
-
-enum FieldType {
-    case text(Validation)
-    case multilineText(Validation)
-    case date
-    case dropdown([String])
-}
-
-extension String {
-    func isValid(for fieldType: FieldType) -> Bool {
-        return !isEmpty
-    }
-}
-
-enum ContactType {
-    case roommate
-    case close
-    case general
-    case other
-}
-
-enum ContactField {
-    case firstName
-    case lastName
-    case phoneNumber
-    case email
-    case relation
-    case birthDate
-    case bsn
-    case profession
-    case companyName
-    case notes
-    
-    var fieldType: FieldType {
-        switch self {
-        case .firstName, .lastName, .companyName:
-            return .text(.name)
-        case .phoneNumber:
-            return .text(.phoneNumber)
-        case .email:
-            return .text(.email)
-        case .relation:
-            return .dropdown(["Vriend of kennis", "Partner"])
-        case .bsn:
-            return .text(.number)
-        case .profession:
-            return .text(.general)
-        case .notes:
-            return .multilineText(.general)
-        case .birthDate:
-            return .date
-        }
-    }
-    
-    var allowsMultiple: Bool {
-        switch self {
-        case .phoneNumber, .email:
-            return true
-        default:
-            return false
-        }
     }
 }
 
