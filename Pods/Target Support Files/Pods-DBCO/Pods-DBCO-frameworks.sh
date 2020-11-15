d120cefda5914bb655e7b3ac1ea94e02821606bc
Thom Hoekstra <thom@egeniq.com>
2020-11-15 16:57:50 +0100

    DBCO-146: Use CocoaLumberjack via Cocoapods

--- Pods/Target Support Files/Pods-DBCO/Pods-DBCO-frameworks.sh
+++ Pods/Target Support Files/Pods-DBCO/Pods-DBCO-frameworks.sh
@@ -197,15 +197,19 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sodium/Sodium.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sodium/Sodium.framework"
 fi
 if [[ "$CONFIGURATION" == "App Store" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sodium/Sodium.framework"
 fi
 if [[ "$CONFIGURATION" == "Ad Hoc" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CocoaLumberjack/CocoaLumberjack.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Sodium/Sodium.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
--- Sources/DBCO/Common/Logging/Logging.swift
+++ Sources/DBCO/Common/Logging/Logging.swift
@@ -5,7 +5,7 @@
  *  SPDX-License-Identifier: EUPL-1.2
  */
 
-import CocoaLumberjackSwift
+import CocoaLumberjack
 import Foundation
 
 public protocol Logging {
