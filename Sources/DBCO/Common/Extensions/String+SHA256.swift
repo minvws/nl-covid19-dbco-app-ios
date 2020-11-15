b3ae1f8767564bf984af347fbc2d4aea5e9e3de2
Thom Hoekstra <thom@egeniq.com>
2020-11-15 15:43:38 +0100

    DBCO-260: SSL Pinning setup

--- Sources/DBCO/Common/Extensions/String+SHA256.swift
+++ Sources/DBCO/Common/Extensions/String+SHA256.swift
@@ -30,3 +30,17 @@ extension String {
     }
     
 }
+
+extension Data {
+    
+    var sha256: Data {
+        var hash = [UInt8](repeating: 0, count: Int(CC_SHA256_DIGEST_LENGTH))
+        
+        withUnsafeBytes {
+            _ = CC_SHA256($0.baseAddress, CC_LONG(count), &hash)
+        }
+        
+        return Data(hash)
+    }
+    
+}
--- Sources/DBCO/Services/NetworkManager/NetworkManagerURLSessionDelegate.swift
+++ Sources/DBCO/Services/NetworkManager/NetworkManagerURLSessionDelegate.swift
@@ -8,50 +8,49 @@
 import Foundation
 import Security
 
-final class NetworkManagerURLSessionDelegate: NSObject, URLSessionDelegate {
+final class NetworkManagerURLSessionDelegate: NSObject, URLSessionDelegate, Logging {
+    let loggingCategory = "NetworkManagerURLSessionDelegate"
+    
     /// Initialise session delegate with certificate used for SSL pinning
     init(configuration: NetworkConfiguration) {
         self.configuration = configuration
     }
+    
+    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> ()) {
 
+        guard let localSignature = configuration.sslSignature(forHost: challenge.protectionSpace.host),
+            challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
+            let serverTrust = challenge.protectionSpace.serverTrust else {
+            // no pinning
+            completionHandler(.performDefaultHandling, nil)
+            return
+        }
+
+        let policies = [SecPolicyCreateSSL(true, challenge.protectionSpace.host as CFString)]
+        SecTrustSetPolicies(serverTrust, policies as CFTypeRef)
+
+        let certificateCount = SecTrustGetCertificateCount(serverTrust)
+
+        guard
+            SecTrustEvaluateWithError(serverTrust, nil),
+            certificateCount > 0,
+            let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, certificateCount - 1), // get topmost certificate in chain
+            let signature = Certificate(certificate: serverCertificate).signature else {
     
-    // To be implemented at a later point
-    
-    
-//    func urlSession(_ session: URLSession, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> ()) {
-//
-//        guard let localSignature = configuration.sslSignature(forHost: challenge.protectionSpace.host),
-//            challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust,
-//            let serverTrust = challenge.protectionSpace.serverTrust else {
-//            // no pinning
-//            completionHandler(.performDefaultHandling, nil)
-//            return
-//        }
-//
-//        let policies = [SecPolicyCreateSSL(true, challenge.protectionSpace.host as CFString)]
-//        SecTrustSetPolicies(serverTrust, policies as CFTypeRef)
-//
-//        let certificateCount = SecTrustGetCertificateCount(serverTrust)
-//
-//        guard
-//            SecTrustEvaluateWithError(serverTrust, nil),
-//            certificateCount > 0,
-//            let serverCertificate = SecTrustGetCertificateAtIndex(serverTrust, certificateCount - 1), // get topmost certificate in chain
-//            let signature = Certificate(certificate: serverCertificate).signature else {
-//            // invalid server trust
-//            completionHandler(.cancelAuthenticationChallenge, nil)
-//            return
-//        }
-//
-//        guard localSignature == signature else {
-//            // signatures don't match
-//            completionHandler(.cancelAuthenticationChallenge, nil)
-//            return
-//        }
-//
-//        // all good
-//        completionHandler(.useCredential, URLCredential(trust: serverTrust))
-//    }
+            logError("Invalid server trust")
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+
+        guard localSignature == signature else {
+            logError("Certificate signatures don't match")
+            completionHandler(.cancelAuthenticationChallenge, nil)
+            return
+        }
+
+        // all good
+        completionHandler(.useCredential, URLCredential(trust: serverTrust))
+    }
 
     // MARK: - Private
 
