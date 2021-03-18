2239e7776fc05945f4c831390dfda24252e2211a
Thom Hoekstra <thom@egeniq.com>
2021-03-18 14:34:23 +0100

    SIG: Small code improvements

---
+++ Sources/DBCO/Common/Extensions/String+Capitalize.swift
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+ *  Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+ *
+ *  SPDX-License-Identifier: EUPL-1.2
+ */
+
+import Foundation
+
+extension String {
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).capitalized + dropFirst()
+    }
+
+    mutating func capitalizeFirstLetter() {
+        self = self.capitalizingFirstLetter()
+    }
+}
