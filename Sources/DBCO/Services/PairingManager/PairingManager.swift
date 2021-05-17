093d2bf9a82f26395faf851d1d0299167878d4ff
Thom Hoekstra <thom@egeniq.com>
2021-05-17 12:01:06 +0200

    DBCO-488: Split Case and Pairing manager files

--- Sources/DBCO/Services/PairingManager/PairingManager.swift
+++ Sources/DBCO/Services/PairingManager/PairingManager.swift
@@ -8,59 +8,6 @@
 import Foundation
 import Sodium
 
-enum PairingManagingError: Error {
-    case alreadyPaired
-    case notPaired
-    case encryptionError
-    case couldNotPair(NetworkError)
-    case pairingCodeExpired
-    case pairingCancelled
-}
-
-protocol PairingManagerListener: AnyObject {
-    func pairingManagerDidStartPollingForPairing(_ pairingManager: PairingManaging)
-    func pairingManager(_ pairingManager: PairingManaging, didFailWith error: PairingManagingError)
-    func pairingManagerDidCancelPollingForPairing(_ pairingManager: PairingManaging)
-    func pairingManager(_ pairingManager: PairingManaging, didReceiveReversePairingCode code: String)
-    func pairingManagerDidFinishPairing(_ pairingManager: PairingManaging)
-}
-
-/// Handles pairing (exchanging keys via libSodium) with the api.
-///
-/// # See also
-/// [Sealed](x-source-tag://Sealed): Used for encrypting and decrypting data after pairing
-///
-/// - Tag: PairingManaging
-protocol PairingManaging {
-    init()
-    
-    var isPaired: Bool { get }
-    var isPollingForPairing: Bool { get }
-    
-    /// Returns the token used to construct the URL for sending and fetching case data
-    /// Throws an `notPaired` error when called befored paired.
-    func caseToken() throws -> String
-    
-    func pair(pairingCode: String, completion: @escaping (_ success: Bool, _ error: PairingManagingError?) -> Void)
-    
-    /// Clears all stored data. Using any method or property except for `isPaired` on PairingManager before pairing again is an invalid operation.
-    func unpair()
-    
-    func startPollingForPairing()
-    func stopPollingForPairing()
-    
-    var canResumePolling: Bool { get }
-    var lastPairingCode: String? { get }
-    var lastPollingError: PairingManagingError? { get }
-    
-    /// Adds a listener
-    /// - parameter listener: The object conforming to [PairingManagerListener](x-source-tag://PairingManagerListener) that will receive updates. Will be stored with a weak reference
-    func addListener(_ listener: PairingManagerListener)
-    
-    func seal<T: Encodable>(_ value: T) throws -> (ciperText: String, nonce: String)
-    func open<T: Decodable>(cipherText: String, nonce: String) throws -> T
-}
-
 /// # See
 /// [PairingManaging](x-source-tag://PairingManaging)
 /// - Tag: PairingManager
