413471da5efedfa917693165e250276892e6fdf2
Thom Hoekstra <thom@egeniq.com>
2021-08-05 10:29:39 +0200

    DBCO-2062: Add Testflight Usertest configuration

--- Pods/Target Support Files/Pods-DBCOUITests/Pods-DBCOUITests-frameworks.sh
+++ Pods/Target Support Files/Pods-DBCOUITests/Pods-DBCOUITests-frameworks.sh
@@ -195,6 +195,9 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CucumberSwift/CucumberSwift.framework"
 fi
+if [[ "$CONFIGURATION" == "Testflight Usertest" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/CucumberSwift/CucumberSwift.framework"
+fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
 fi
